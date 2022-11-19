c406f0ee1b4a685706760f7464b780bba0f1f122
i.chiesov <i.chiesov@gmail.com>
2022-11-19 11:20:04 +0700

    precommit

--- src/main/java/refcursorconnector/RefCursorConnector.java
+++ src/main/java/refcursorconnector/RefCursorConnector.java
@@ -1,17 +1,24 @@
 package refcursorconnector;
 
+import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.framework.common.objects.Attribute;
 import org.identityconnectors.framework.common.objects.ObjectClass;
 import org.identityconnectors.framework.common.objects.OperationOptions;
 import org.identityconnectors.framework.common.objects.Uid;
 import org.identityconnectors.framework.spi.Configuration;
 import org.identityconnectors.framework.spi.Connector;
+import org.identityconnectors.framework.spi.ConnectorClass;
 import org.identityconnectors.framework.spi.operations.CreateOp;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.Set;
 
+@ConnectorClass(
+        displayNameKey = "REFCURSOR_CONNECTOR",
+        configurationClass = RefCursorConfiguration.class)
 public class RefCursorConnector implements Connector, CreateOp {
+
+
+    public static final Log LOG = Log.getLog(RefCursorConnector.class);
 
     private RefCursorConfiguration configuration;
     private RefCursorConnection connection;
@@ -39,7 +46,12 @@ public class RefCursorConnector implements Connector, CreateOp {
 
     @Override
     public Uid create(ObjectClass objectClass, Set<Attribute> set, OperationOptions operationOptions) {
+        LOG.info("attributes {0}", set);
+        String name = (String) set.toArray(new Attribute[0])[0].getValue().get(0);
+        LOG.info("name {0}", name);
+
         // TODO implement
-        throw new NotImplementedException();
+
+        return new Uid(name);
     }
 }
